a5e73145fa5eb1b5b54319c324d957509b8d3b14
Matias DEVEZE <matiasdeveze@gmail.com>
2025-02-22 12:01:41 +0100

    add model for movies table

---
+++ iut-project/lib/models/movies.js
@@ -0,0 +1,33 @@
+// lib/models/movie.js
+'use strict';
+
+const Joi = require('joi');
+const { Model } = require('@hapipal/schwifty');
+
+module.exports = class Movie extends Model {
+
+    static get tableName() {
+        return 'movies';
+    }
+
+    static get joiSchema() {
+        return Joi.object({
+            id: Joi.number().integer().greater(0),
+            title: Joi.string().min(1).required(),
+            description: Joi.string().required(),
+            releaseDate: Joi.date().required(),
+            director: Joi.string().required(),
+            createdAt: Joi.date(),
+            updatedAt: Joi.date()
+        });
+    }
+
+    $beforeInsert(queryContext) {
+        this.updatedAt = new Date();
+        this.createdAt = this.updatedAt;
+    }
+
+    $beforeUpdate(opt, queryContext) {
+        this.updatedAt = new Date();
+    }
+};
