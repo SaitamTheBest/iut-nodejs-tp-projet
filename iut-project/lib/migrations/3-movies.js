b59b00db07b61c88edf08aa78e4d64292985d80b
Matias DEVEZE <matiasdeveze@gmail.com>
2025-02-22 11:59:28 +0100

    create migration movies

---
+++ iut-project/lib/migrations/3-movies.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+
+    async up(knex) {
+
+        await knex.schema.createTable('movies', (table) => {
+
+            table.increments('id').primary();
+            table.string('title').notNull();
+            table.string('description').notNull();
+            table.date('publishedAt').notNull();
+            table.string('producer').notNull();
+
+            table.dateTime('createdAt').notNull().defaultTo(knex.fn.now());
+            table.dateTime('updatedAt').notNull().defaultTo(knex.fn.now());
+        });
+    },
+
+    async down(knex) {
+
+        await knex.schema.dropTableIfExists('movies');
+    }
+};
